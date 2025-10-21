62732ca36a52812242e10bd1ac76b1b523dde656
london-codes <london.powell.work@gmail.com>
2025-10-20 21:30:35 -0700

    getting rid of wnba data

--- src/list_players_update.py
+++ src/list_players_update.py
@@ -16,31 +16,4 @@ Important Notes:
 """
     #   updates:
 
-#LVA
-update_player_csv('https://www.basketball-reference.com/wnba/players/w/wilsoa01w/gamelog/2025/', 'aja')
-update_player_csv('https://www.basketball-reference.com/wnba/players/g/graych01w/gamelog/2025/', 'chelsea')
-update_player_csv('https://www.basketball-reference.com/wnba/players/l/loydje01w/gamelog/2025/', 'jewell')
-update_player_csv('https://www.basketball-reference.com/wnba/players/s/smithna01w/gamelog/2025/', 'nalyssa')
-update_player_csv('https://www.basketball-reference.com/wnba/players/y/youngja01w/gamelog/2025/', 'jackie')
 
-#PHO
-update_player_csv('https://www.basketball-reference.com/wnba/players/s/sabalsa01w/gamelog/2025/','satou')
-update_player_csv('https://www.basketball-reference.com/wnba/players/w/whitcsa01w/gamelog/2025/','sami')
-update_player_csv('https://www.basketball-reference.com/wnba/players/a/akoamo01w/gamelog/2025/','monique')
-update_player_csv('https://www.basketball-reference.com/wnba/players/c/coppeka01w/gamelog/2025/','kahleah')
-update_player_csv('https://www.basketball-reference.com/wnba/players/b/bonnede01w/gamelog/2025/','dewanna')
-update_player_csv('https://www.basketball-reference.com/wnba/players/t/thomaal01w/gamelog/2025/','alyssa')
-
-#MIN
-update_player_csv('https://www.basketball-reference.com/wnba/players/h/hiedena01w/gamelog/2025/','natisha')
-update_player_csv('https://www.basketball-reference.com/wnba/players/c/collina01w/gamelog/2025/','napheesa')
-update_player_csv('https://www.basketball-reference.com/wnba/players/s/smithal01w/gamelog/2025/','alanna')
-update_player_csv('https://www.basketball-reference.com/wnba/players/c/carlebr01w/gamelog/2025/','bridget')
-update_player_csv('https://www.basketball-reference.com/wnba/players/m/mcbrika01w/gamelog/2025/','kayla')
-
-#IND
-update_player_csv('https://www.basketball-reference.com/wnba/players/s/simsod01w/gamelog/2025/','odyssey')
-update_player_csv('https://www.basketball-reference.com/wnba/players/b/bostoal01w/gamelog/2025/','aliyah')
-update_player_csv('https://www.basketball-reference.com/wnba/players/m/mitchke01w/gamelog/2025/','kelsey')
-update_player_csv('https://www.basketball-reference.com/wnba/players/h/hullle01w/gamelog/2025/','lexie')
-update_player_csv('https://www.basketball-reference.com/wnba/players/h/howarna01w/gamelog/2025/','natasha')
