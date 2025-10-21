e7c6cf70c6b7e1947e5981defe7a86ad48ff57b2
london-codes <london.powell.work@gmail.com>
2025-10-20 23:25:11 -0700

    added some players in and fixed main so it takes into account PAR PA PR

--- src/list_players_update.py
+++ src/list_players_update.py
@@ -16,4 +16,20 @@ Important Notes:
 """
     #   updates:
 
+#Golden state
 update_player_csv('https://www.basketball-reference.com/players/c/curryst01/gamelog/2025','curry')
+update_player_csv('https://www.basketball-reference.com/players/b/butleji01/gamelog/2025','butler')
+update_player_csv('https://www.basketball-reference.com/players/g/greendr01/gamelog/2025','draymond')
+update_player_csv('https://www.basketball-reference.com/players/k/kuminjo01/gamelog/2025','jonathan')
+update_player_csv('https://www.basketball-reference.com/players/p/podzibr01/gamelog/2025','brandin')
+update_player_csv('https://www.basketball-reference.com/players/h/hieldbu01/gamelog/2025','buddy')
+
+#okc
+update_player_csv('https://www.basketball-reference.com/players/g/gilgesh01/gamelog/2025','shai')
+update_player_csv('https://www.basketball-reference.com/players/d/dortlu01/gamelog/2025','luguentz')
+update_player_csv('https://www.basketball-reference.com/players/w/wiggiaa01/gamelog/2025','aaron')
+update_player_csv('https://www.basketball-reference.com/players/h/holmgch01/gamelog/2025','chet')
+update_player_csv('https://www.basketball-reference.com/players/w/wallaca01/gamelog/2025','cason')
+update_player_csv('https://www.basketball-reference.com/players/c/carusal01/gamelog/2025','alex')
+update_player_csv('https://www.basketball-reference.com/players/h/harteis01/gamelog/2025','isaiah')
+
